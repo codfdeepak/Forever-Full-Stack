10fc0f9558a326513ed5e323dfa25507b3646784
Deepak kumar <codfdeepak@gmail.con>
2026-01-12 12:02:52 +0530

    updated

--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -4,37 +4,37 @@ import mongoose from "mongoose"
 const orderSchema = new mongoose.Schema({
   userId: {
     type: String,
-    requied:true
+    required:true
   },
   items: {
     type: Array,
-    requied:true
+    required:true
   },
   amount: {
     type: Number,
-    requied:true
+    required:true
   },
   address: {
     type: Object,
-    requied:true
+    required:true
   },
   status: {
     type: String,
-    requied: true,
+    required: true,
     default:'Order Placed'
   },
   paymentMethod: {
     type: String,
-    requied:true
+    required:true
   },
   payment: {
     type: Boolean,
-    requied: true,
+    required: true,
     default:false
   },
   date: {
     type: Number,
-    requied:true
+    required:true
   }
 
   
